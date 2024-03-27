d1fa54bca6d605c842f5f6d41eecc215cd4cf84c
Adhe <adhepluk@gmail.com>
2024-03-27 10:01:45 +0700

    Update Photo speaker

--- src/lib/components/Nav.svelte
+++ src/lib/components/Nav.svelte
@@ -1,34 +1,35 @@
 <script>
-	import Link from '$lib/components/Link.svelte';
+  import Link from '$lib/components/Link.svelte';
 
-	export let url;
-	let isNavAppear = false;
+  export let url;
+  let isNavAppear = false;
 
-	function handleScroll () {
-		const currentScrollPosition = window.scrollY || document.documentElement.scrollTop;
-		if (currentScrollPosition > 100) {
-			console.log('Navbar appear')
-			isNavAppear = true;
-		} else {
-			console.log('nav delete')
-			isNavAppear = false;
-		}
-
-	}
+  function handleScroll() {
+    const currentScrollPosition = window.scrollY || document.documentElement.scrollTop;
+    if (currentScrollPosition > 100) {
+      console.log('Navbar appear');
+      isNavAppear = true;
+    } else {
+      console.log('nav delete');
+      isNavAppear = false;
+    }
+  }
 </script>
 
 <svelte:window on:scroll={handleScroll} />
 
-<div class={`${isNavAppear ? 'ease-out' : '-translate-y-full ease-in-out'} fixed top-0 inset-x-0 drop-shadow-md  backdrop-blur-xl bg-white/85 transition duration-300 z-20`}>
-	<div class="container w-4/5 flex items-center justify-between py-3 mx-auto">
-		<img src="metaits-logo-black.svg" class="h-12 " alt="" />
-		<div class="flex">
-			<Link href={url + '#'}>Home</Link>
-			<Link href={url + '/#topic'}>Topics</Link>
-			<Link href={url + '/#keynote-speaker'}>Keynote Speaker</Link>
-			<Link href={url + '/#submissions'}>Submissions</Link>
-			<Link href={url + '/#'}>Registration</Link>
-			<Link href={url + '/#contact'}>Contact</Link>
-		</div>
-	</div>
+<div
+  class={`${isNavAppear ? 'ease-out' : '-translate-y-full ease-in-out'} fixed top-0 inset-x-0 drop-shadow-md  backdrop-blur-xl bg-white/85 transition duration-300 z-20`}
+>
+  <div class="container w-4/5 flex items-center justify-between py-3 mx-auto">
+    <img src="metaits-logo-black.svg" class="h-12" alt="" />
+    <div class="flex">
+      <Link href={url + '#'}>Home</Link>
+      <Link href={url + '/#topic'}>Topics</Link>
+      <Link href={url + '/#keynote-speaker'}>Keynote Speaker</Link>
+      <Link href={url + '/#submissions'}>Submissions</Link>
+      <Link href={url + '/#'}>Registration</Link>
+      <Link href={url + '/#contact'}>Contact</Link>
+    </div>
+  </div>
 </div>
--- src/lib/data.js
+++ src/lib/data.js
@@ -83,7 +83,7 @@ export const Speakers = [
 			'Building National Competence in Quantum  Information Science and Technology: Yes, We Can',
 		about:
 			'In 1988 I did my master thesis in Nijmegen at the Orthopaedic Research Laboratory(ORL). After my graduation mid- 1989 I stayed at the ORL as Junior Researcher and performed various kinds of projects focusing on testing of orthopaedic implants. Most projects were funded by the Orthopaedic Industry which gradually led to an extensive network of R&D departments of small to large international orthopaedic companies. The publications of these projects resulted in my Ph.D. degree in 1995. Subsequently I was promoted to Assistant Professor and was appointed to be Director of Pre-clinical testing of joint implants. In 2003 I was further promoted to Associate Professor. Since February 2014 I am a full Professor at the Radboudumc Institute for Health Sciences (RIHS), my theme is Reconstructive and regenerative medicine. My research interests are focused on orthopaedic- biomechanical problems of the lower extremity.Our group utilizes computer modelling techniques in combination with cadaveric testing, animal research and clinical studies in order to optimize diagnostic and evaluation tools for orthopaedic patients.We work with researchers and orthopaedic companies to optimize the functionality and longevity of orthopaedic implants such as knee and hip prostheses.Furthermore, we focus on the calculation of bone strength for patients that have osteoporosis or metastatic lesions. Since October 2007 I have a part - time appointment(1 day per week) as Professor in Implant Biomechanics at the University of Twente(dept.Biomechanical Engineering headed by Prof.Bart Koopman), where I am setting up a research line related to biomechanics of the Musculo - Skeletal system, see TLEMsafe(www.tlemsafe.eu /). In December 2012 I was awarded a "European Research Council - Advanced Grant" for follow up research.In Nijmegen we are currently expanding our modelling efforts to generate patient specific models and feed these models with innovative dynamic imaging techniques(see www.erc - biomechtools.eu). In 2016 I was appointed as a visiting professor at the Politecnico di Milano(Italy). In 2018 I received an Honorary Doctorate of Aalborg University(Denmark)',
-		photoUrl: 'Photo-Dr-Wirawan.jpg'
+		photoUrl: 'verdonschot-nicco-min.jpg'
 	},
 	{
 		id: 2,
@@ -93,7 +93,7 @@ export const Speakers = [
 			'Building Resilient Power Distribution System: Research Opportunities and Potential Industrial Collaboration',
 		about:
 			'Prof  Rameen Shakur MD Ph.D. is an academic clinician-scientist and molecular biologist by training specializing in molecular mechanisms of regeneration and applications of precision medicine for cardiovascular disease at the Massachusetts Institute of Technology, Boston USA. Prof Shakur undertook his undergraduate studies at the University of Cambridge, UK where he completed his MPhil in Immunology, and completed his medical studies at the University of Edinburgh. He completed his clinical training at the John Radcliffe Hospital, University of Oxford, UK. He undertook his general medical and cardiology training in London at St Bartholomews Hospital, The London Chest Hospital, and Cardiac imaging at the Royal Brompton Hospital, London. He was awarded the Winston Churchill fellowship at Harvard medical school where he undertook work in Prof Peter Libbys lab and also at the Mayo Clinic, Rochester at Prof Mike Ackermans lab. He completed his Ph.D. at the University of Cambridge at the Wellcome Trust Sanger centre and regenerative medicine laboratory. During his Ph.D., he was awarded the Isabelle Buohon award to undertake work on genome editing in human IPS cells at Prof George Churchs lab at Harvard. He undertook his post-doctoral work at Prof Bob Langers lab as the inaugural Janson fellow at the Koch institute for integrative cancer research at MIT. His work centers upon the delineation of the mechanism of action of inflammation and modulation of connexins and therapies for drug discovery in cardiovascular disease. He is also been instrumental in a number of important papers on novel models for the phenotype-genotype disparities in inherited cardiomyopathies.',
-		photoUrl: 'Photo-Prof-Hazlie-Mokhlis.jpg'
+		photoUrl: 'rameen-shakur-min.jpg'
 	},
 	{
 		id: 3,
@@ -102,7 +102,7 @@ export const Speakers = [
 		paperTitle: 'Applications of AI in Communication Systems and Industrial Collaborations',
 		about:
 			'Dr. Eng. Dhany Arifianto, ST., M.Eng received his Ph.D. degree in the Department of Information Processing from the university of Tokyo Institute of Technology, Japan in 2005. He is currently working as an associate professor and the lecturer in the Medical Technology program study, Institut Teknologi Sepuluh Nopember, Indonesia. His research interests include Acoustical Signal Processing, Auditory Neuroscience, Vibration Analysis etc.',
-		photoUrl: 'prof-wan-jen-huang.jpg'
+		photoUrl: 'dhanny-a-min.jpg'
 	}
 ];
 
